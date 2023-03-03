92eda626bf09cc12d67686d455eac23299c68e0c
SethCommichaux <yofi01@gmail.com>
2023-03-03 11:30:24 -0500

    Update fastq_read_count.py

--- fastq_read_count.py
+++ fastq_read_count.py
@@ -4,25 +4,24 @@ import gzip
 from multiprocessing import Process
 
 def count_reads(fastq):
-        with open('read_counts.txt','a') as out:
-                c = 0
-                total_rd_len = 0
-                if fastq.endswith('.gz'):
-                        for h,i in enumerate(gzip.open(fastq, "rt")):
-                                if h%4 == 1:
-                                        total_rd_len += len(i.strip())
-                                        c += 1
-                else:
-                        for h,i in enumerate(open(fastq)):
-                                if h%4 == 1:
-                                        total_rd_len += len(i.strip())
-                                        c += 1
-                out.write(fastq.split('/')[-1]+'\t'+str(c)+'\t'+str(int(total_rd_len/c))+'\n')
+       c = 0
+       total_rd_len = 0
+       if fastq.endswith('.gz'):
+               for h,i in enumerate(gzip.open(fastq, "rt")):
+                       if h%4 == 1:
+                               total_rd_len += len(i.strip())
+                               c += 1
+       else:
+               for h,i in enumerate(open(fastq)):
+                       if h%4 == 1:
+                               total_rd_len += len(i.strip())
+                               c += 1
+       out.write(fastq.split('/')[-1]+'\t'+str(c)+'\t'+str(int(total_rd_len/c))+'\n')
 
 file_of_files = sys.argv[1]
 
-for f in open(file_of_files):
-        p = Process(target=count_reads, args=(f.strip(),))
-        p.start()
-
-p.join()
+with open('read_counts.txt','w') as out:
+        for f in open(file_of_files):
+                p = Process(target=count_reads, args=(f.strip(),))
+                p.start()
+        p.join()
