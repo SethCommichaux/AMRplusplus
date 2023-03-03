e65cdf9790b443f4883df911c39685c79e0327db
SethCommichaux <yofi01@gmail.com>
2023-03-03 11:19:50 -0500

    Update fastq_read_count.py

--- fastq_read_count.py
+++ fastq_read_count.py
@@ -6,28 +6,17 @@ from multiprocessing import Process
 def count_reads(fastq):
         with open('read_counts.txt','a') as out:
                 c = 0
-                mean_rd_len = []
-
+                total_rd_len = 0
                 if fastq.endswith('.gz'):
-                        with gzip.open(fastq, "rt") as handle:
-                                for h,i in enumerate(SeqIO.parse(handle,'fastq')):
-                                        mean_rd_len.append(len(i.seq))
-                                        if h == 10000:
-                                                break
-                        with gzip.open(fastq, "rt") as handle:
-                                for i in handle:
-                                        if i[0] == '+':
-                                                c += 1
-                else:
-                        for h,i in enumerate(SeqIO.parse(fastq,'fastq')):
-                                mean_rd_len.append(len(i.seq))
-                                if h == 10000:
-                                        break
-
-                        for i in open(fastq):
-                                if i[0] == '+':
+                        for h,i in enumerate(gzip.open(fastq, "rt")):
+                                if h%4 == 1:
+                                        total_rd_len += len(i.strip())
                                         c += 1
-
+                else:
+                        for h,i in enumerate(open(fastq)):
+                                if h%4 == 1:
+                                        total_rd_len += len(i.strip())
+                                        c += 1
                 out.write(fastq.split('/')[-1]+'\t'+str(c)+'\t'+str(int(sum(mean_rd_len)/len(mean_rd_len)))+'\n')
 
 file_of_files = sys.argv[1]
